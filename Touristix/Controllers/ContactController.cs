8fe033e41679b2afa7ce47ba41f0627a51eaa065
eric2121 <e_casault@hotmail.com>
2014-09-05 16:20:01 -0400

    ContactCree

---
+++ Touristix/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Touristix.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        [HttpGet]
+        public ActionResult ContactForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ContactForm(string Nom)
+        {
+            return View();
+        }
+
+    }
+}
