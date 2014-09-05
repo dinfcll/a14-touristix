387aeef7e30df080b07e03934e22092b109c700a
BrousseauDemersB <Wargamer3@hotmail.com>
2014-09-05 14:33:44 -0400

    Ajout Destination
    Ajout de Model, Controller et base de données Destination.

---
+++ Touristix/Models/DestinationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Touristix.Models
+{
+    public class DestinationModel
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public string Pays { get; set; }
+        public string Region { get; set; }
+        public string Ville { get; set; }
+    }
+
+    public class DestinationDBContext : DbContext
+    {
+        public DbSet<DestinationModel> Destinations { get; set; }
+    }
+}
