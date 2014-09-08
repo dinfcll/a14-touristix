df31c0eefff3394059309ee55f4263b34ed02ec8
BrousseauDemersB <Wargamer3@hotmail.com>
2014-09-08 10:53:13 -0400

    Traduction du projet en français
    Les textes et classes on été traduit en français.

--- Touristix/Controllers/DestinationController.cs
+++ Touristix/Controllers/DestinationController.cs
@@ -15,20 +15,19 @@ namespace Touristix.Controllers
 
         //
         // GET: /Destination/
-
         public ActionResult Index(string DestinationNom = "", string DestinationPays = "", string DestinationRegion = "", int Trier = 0)
         {
             IQueryable<DestinationModel> Destinations = from m in db.Destinations
                                                         select m;
 
             if (!string.IsNullOrEmpty(DestinationNom))
-                RechercheParNom(ref Destinations, DestinationNom);
+                Destinations = Destinations.Where(s => s.Nom.Contains(DestinationNom));
 
             if (!string.IsNullOrEmpty(DestinationPays))
-                RechercheParPays(ref Destinations, DestinationPays);
+                Destinations = Destinations.Where(s => s.Pays.Contains(DestinationPays));
 
             if (!string.IsNullOrEmpty(DestinationRegion))
-                RechercheParRegion(ref Destinations, DestinationRegion);
+                Destinations = Destinations.Where(s => s.Region.Contains(DestinationRegion));
 
             IQueryable<IGrouping<string, DestinationModel>> DestinationRecu;
 
@@ -54,39 +53,8 @@ namespace Touristix.Controllers
             return View(new Tuple<DestinationModel[], IQueryable<IGrouping<string, DestinationModel>>>(Array5DerniereDestination, DestinationRecu));
         }
 
-        public IQueryable<DestinationModel> RechercheParNom(ref IQueryable<DestinationModel> Destinations, string DestinationNom)
-        {
-            if (!String.IsNullOrEmpty(DestinationNom))
-            {
-                Destinations = Destinations.Where(s => s.Nom.Contains(DestinationNom));
-            }
-
-            return Destinations;
-        }
-
-        public IQueryable<DestinationModel> RechercheParPays(ref IQueryable<DestinationModel> Destinations, string DestinationPays)
-        {
-            if (!String.IsNullOrEmpty(DestinationPays))
-            {
-                Destinations = Destinations.Where(s => s.Pays.Contains(DestinationPays));
-            }
-
-            return Destinations;
-        }
-
-        public IQueryable<DestinationModel> RechercheParRegion(ref IQueryable<DestinationModel> Destinations, string DestinationRegion)
-        {
-            if (!String.IsNullOrEmpty(DestinationRegion))
-            {
-                Destinations = Destinations.Where(s => s.Region.Contains(DestinationRegion));
-            }
-
-            return Destinations;
-        }
-
         //
         // GET: /Destination/Admin
-
         public ActionResult Admin()
         {
             return View(db.Destinations.ToList());
@@ -94,7 +62,6 @@ namespace Touristix.Controllers
 
         //
         // GET: /Destination/Information/
-
         public ActionResult Information(int id = 0)
         {
             DestinationModel destinationmodel = db.Destinations.Find(id);
@@ -107,23 +74,21 @@ namespace Touristix.Controllers
 
         //
         // GET: /Destination/Create
-
-        public ActionResult Create()
+        public ActionResult Creer()
         {
             return View();
         }
 
         //
         // POST: /Destination/Create
-
         [HttpPost]
-        public ActionResult Create(DestinationModel destinationmodel)
+        public ActionResult Creer(DestinationModel destinationmodel)
         {
             if (ModelState.IsValid)
             {
                 db.Destinations.Add(destinationmodel);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Admin");
             }
 
             return View(destinationmodel);
@@ -131,8 +96,7 @@ namespace Touristix.Controllers
 
         //
         // GET: /Destination/Edit/5
-
-        public ActionResult Edit(int id = 0)
+        public ActionResult Modifier(int id = 0)
         {
             DestinationModel destinationmodel = db.Destinations.Find(id);
             if (destinationmodel == null)
@@ -144,9 +108,8 @@ namespace Touristix.Controllers
 
         //
         // POST: /Destination/Edit/5
-
         [HttpPost]
-        public ActionResult Edit(DestinationModel destinationmodel)
+        public ActionResult Modifier(DestinationModel destinationmodel)
         {
             if (ModelState.IsValid)
             {
@@ -159,8 +122,7 @@ namespace Touristix.Controllers
 
         //
         // GET: /Destination/Delete/5
-
-        public ActionResult Delete(int id = 0)
+        public ActionResult Supprimer(int id = 0)
         {
             DestinationModel destinationmodel = db.Destinations.Find(id);
             if (destinationmodel == null)
@@ -172,8 +134,7 @@ namespace Touristix.Controllers
 
         //
         // POST: /Destination/Delete/5
-
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName("Supprimer")]
         public ActionResult DeleteConfirmed(int id)
         {
             DestinationModel destinationmodel = db.Destinations.Find(id);
--- Touristix/Models/DestinationModel.cs
+++ Touristix/Models/DestinationModel.cs
@@ -21,10 +21,48 @@ namespace Touristix.Models
         public string Ville { get; set; }
         [Required]
         public string Image { get; set; }
+        [Required]
+        public string Apercu { get; set; }
+        [Required]
+        public string Details { get; set; }
+    }
+
+    public class BatimentModel
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string Apercu { get; set; }
+        [Required]
+        public string Details { get; set; }
+
+        public string URL { get; set; }
+    }
+
+    public class ActiviteModel
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string Lieu { get; set; }
+        [Required]
+        public string Apercu { get; set; }
+        [Required]
+        public string Details { get; set; }
     }
 
     public class DestinationDBContext : DbContext
     {
         public DbSet<DestinationModel> Destinations { get; set; }
+        public DbSet<BatimentModel> Batiments { get; set; }
+        public DbSet<ActiviteModel> Activites { get; set; }
     }
 }
