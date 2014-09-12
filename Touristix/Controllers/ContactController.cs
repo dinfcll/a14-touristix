842f7d652550ca504e8d5ce16a0dd28a9fbc8975
eric2121 <e_casault@hotmail.com>
2014-09-12 12:53:19 -0400

    email bug
    un email qui bug

--- Touristix/Controllers/ContactController.cs
+++ Touristix/Controllers/ContactController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Touristix.Models;
+using System.Net.Mail;
 
 namespace Touristix.Controllers
 {
@@ -18,10 +20,41 @@ namespace Touristix.Controllers
         }
 
         [HttpPost]
-        public ActionResult ContactForm(string Nom)
+        public ActionResult ContactForm(ContactModel modele)
         {
+            if (ModelState.IsValid)
+            {
+                InsererContact(modele.Nom, modele.Courriel, modele.Categorie, modele.Commentaires);
+                TempData["notice"] = "Votre formulaire a été soumis";
+                return RedirectToAction("Index", "Home");
+            }
             return View();
+            
         }
+        private void InsererContact(string nom, string courriel,  string categorie, string commentaires)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                client.Port = 587;
+                client.Host = "smtp.gmail.com";                
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("touristix21@gmail.com", "qazedctgb");
 
+                MailMessage message = new MailMessage(courriel, "lauwarrior@yahoo.ca", categorie, commentaires);
+                message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                
+                client.Send(message);
+            }
+            catch (SmtpException sex)
+            {
+                string a = sex.Message;
+            }
+
+                            
+            
+        }
     }
 }
