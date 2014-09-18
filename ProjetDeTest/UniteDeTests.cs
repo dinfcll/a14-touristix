55107a541a262c45dc7d846c5730ff69f1018000
lorenzo21 <lauwarrior@yahoo.ca>
2014-09-18 19:48:10 -0400

    Projet de test
    Tests débutés

---
+++ ProjetDeTest/UniteDeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Touristix.Controllers;
+using System.Web.Mvc;
+using Touristix.Models;
+
+namespace ProjetDeTest
+{
+    [TestClass]
+    public class UniteDeTests
+    {
+        [TestMethod]
+        public void TestOuvrirAccueilControlleur()
+        {
+            var controller = new AccueilController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
