74bcf9255e69e72a54df4e7fe075b101eb9608bc
lorenzo21 <lauwarrior@yahoo.ca>
2014-09-19 16:13:32 -0400

    Création du contrôleur

---
+++ Touristix/Controllers/ConnexionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Touristix.Controllers
+{
+    public class ConnexionController : Controller
+    {
+        //
+        // GET: /Connexion/
+
+        public ActionResult ConnexionAdmin()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Touristix/Filters/InitialiserAttributAdmin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Touristix.Filters
+{
+    public class InitialiserAttributAdmin
+    {
+    }
+}
