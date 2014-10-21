a9b5bdbf002a2afa5d0453cc9b5547b10832c474
BrousseauDemersB <Wargamer3@hotmail.com>
2014-10-21 13:45:12 -0400

    Traduction en français

--- Touristix/Controllers/Admin/AdminController.cs
+++ Touristix/Controllers/Admin/AdminController.cs
@@ -9,34 +9,28 @@ namespace Touristix.Controllers
 {
     public class AdminController : Controller
     {
-        //
-        // GET: /Téléversement/
-
         public ActionResult Index()
         {
             return View();
         }
 
-        public JsonResult Magic()
+        public JsonResult ReceptionImage()
         {
             string[] Images = Directory.GetFiles(Server.MapPath("~/Images/Destinations/"), "*.*");
 
             for (int i = 0; i < Request.Files.Count; i++)
             {
-                // for each file being sent over...
                 HttpPostedFileBase Image = Request.Files[i];
 
                 string CheminComplet = System.IO.Path.Combine(
                                        Server.MapPath("~/Images/Destinations/"), System.IO.Path.GetFileName(Image.FileName));
 
                 if (System.IO.File.Exists(CheminComplet))
-                    return Json("File upload failed");
+                    return Json("Le téléversement a échoué");
 
-                // file is uploaded
                 Image.SaveAs(CheminComplet);
             }
-            // after successfully uploading redirect the user
-            return Json("File uploaded successfully");
+            return Json("Téléversement complété");
         }
     }
 }
