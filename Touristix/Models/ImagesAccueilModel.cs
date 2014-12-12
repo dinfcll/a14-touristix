d2f5515252b5e7208ddb3c4937da6fe0967714aa
lorenzo21 <lauwarrior@yahoo.ca>
2014-12-12 13:38:13 -0500

    Enlever la sélection des images aléatoires Accueil
    Javascript enlevé dans vue Accueil, modèle et controlleur modifié
    (tableau de string retiré) et tests corrigés

--- Touristix/Models/ImagesAccueilModel.cs
+++ Touristix/Models/ImagesAccueilModel.cs
@@ -6,23 +6,8 @@ namespace Touristix.Models
 {
     public class ImagesAccueilModel
     {
-        public string[] TableauImagesAccueil { get; set; }
-
         public List<ALaUneModel> ListALaUne { get; set; }
 
-        public ImagesAccueilModel(string url)
-        {
-            string[] ExtensionsRecherche = { ".png", ".jpg", ".bmp" };
-
-            TableauImagesAccueil = Directory.GetFiles(url, "*.*")
-                .Where(f => ExtensionsRecherche.Contains(new FileInfo(f).Extension.ToLower())).ToArray();
-
-            for (int i = 0; i < TableauImagesAccueil.Length; i++)
-            {
-                string strCheminImage = TableauImagesAccueil[i];
-
-                TableauImagesAccueil[i] = Path.GetFileName(strCheminImage);
-            }
-        }
+        public ImagesAccueilModel(){}
     }
 }
